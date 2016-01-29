9206060593c401352f3b3a28f9a1fc3a24ea7310
Greg Scott <i@gregoryjscott.com>
2016-01-28 20:29:16 -0700

    WIP ls runs but not reading data

--- dist/functions/helloworld/index.js
+++ dist/functions/helloworld/index.js
@@ -2,14 +2,12 @@ var child = require('child_process');
 var path = require('path')
 
 exports.handle = function(e, ctx) {
-  console.log('before helloworld')
-
   var exe = path.join(__dirname, 'helloworld')
-  var proc = child.spawn('exe')
+  var proc = child.spawn('ls')
 
   proc.stdout.on('data', function(data) {
-    console.log('data callback')
-    ctx.succeed({ thanksFor: 'nothing'})
+    ctx.succeed({ didItWork: data})
+    console.log('IT WORKED!')
   });
 
   proc.stderr.on('data', function(error) {
